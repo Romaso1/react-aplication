dd79fde00c8726061eb8ebdcbc22cf1ad34bbae9
Romaso1 <90009050+Romaso1@users.noreply.github.com>
2022-11-11 12:40:08 +0200

    create

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.css';
+import Navigation from './components/navigation';
+import Content from './components/content';
+
+function App () {
+  return (
+    <div className="App">
+      <Navigation />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
